18e9a6676d673db79269954848076deade1cea24
Maksym Mykhailenko <maxceem@gmail.com>
2021-04-05 11:22:16 +0300

    fix: make env variables same like variables
    to avoid confusion

--- config/default.js
+++ config/default.js
@@ -6,7 +6,7 @@ module.exports = {
   AUTH_SECRET: process.env.AUTH_SECRET || 'mysecret',
   VALID_ISSUERS: process.env.VALID_ISSUERS || '["https://api.topcoder-dev.com", "https://api.topcoder.com", "https://topcoder-dev.auth0.com/", "https://auth.topcoder-dev.com/"]',
   AMAZON: {
-    AWS_REGION: process.env.AWS_DEFAULT_REGION || 'us-east-1',
+    AWS_REGION: process.env.AWS_REGION || 'us-east-1',
     DYNAMODB_READ_CAPACITY_UNITS: process.env.DYNAMODB_READ_CAPACITY_UNITS || 10,
     DYNAMODB_WRITE_CAPACITY_UNITS: process.env.DYNAMODB_WRITE_CAPACITY_UNITS || 5,
     IS_LOCAL_DB: process.env.IS_LOCAL_DB ? process.env.IS_LOCAL_DB === 'true' : false,
@@ -15,19 +15,19 @@ module.exports = {
     AWS_ACCESS_KEY_ID: 'local-dummy',
     AWS_SECRET_ACCESS_KEY: 'local-dummy',
     // We also have to provide URL if `IS_LOCAL_DB=true`
-    DYNAMODB_URL: process.env.AWS_DYNAMODB_URL || 'http://localhost:8000'
+    DYNAMODB_URL: process.env.DYNAMODB_URL || 'http://localhost:8000'
   },
 
-  DB_COLLECTION_CHALLENGES: process.env.ENV_SOLUTION_DB_COLLECTION_CHALLENGES || 'challenge_tags',
-  TAGGING_EMSI_TYPE: process.env.ENV_TAGGING_TAGGING_EMSI_TYPE || 'internal_no_refresh',
-  ENABLE_CUSTOM_TAGGING: process.env.ENV_TAGGING_ENABLE_CUSTOM_TAGGING || 'false',
-  UPDATE_LOCAL_BEFORE_TAGGING: process.env.ENV_TAGGING_UPDATE_LOCAL_BEFORE_TAGGING || 'false',
-  EXTRACT_CONFIDENCE: process.env.ENV_TAGGING_EXTRACT_CONFIDENCE || 'false',
-  TEXT_LENGTH: process.env.ENV_TAGGING_TEXT_LENGTH || 300,
+  DB_COLLECTION_CHALLENGES: process.env.DB_COLLECTION_CHALLENGES || 'challenge_tags',
+  TAGGING_EMSI_TYPE: process.env.TAGGING_EMSI_TYPE || 'internal_no_refresh',
+  ENABLE_CUSTOM_TAGGING: process.env.ENABLE_CUSTOM_TAGGING || 'false',
+  UPDATE_LOCAL_BEFORE_TAGGING: process.env.UPDATE_LOCAL_BEFORE_TAGGING || 'false',
+  EXTRACT_CONFIDENCE: process.env.EXTRACT_CONFIDENCE || 'false',
+  TEXT_LENGTH: process.env.TEXT_LENGTH || 300,
 
-  CHALLENGE_BASE_URL: process.env.ENV_CHALLENGE_BASE_URL || 'https://api.topcoder-dev.com',
+  CHALLENGE_BASE_URL: process.env.CHALLENGE_BASE_URL || 'https://api.topcoder-dev.com',
 
-  TAGGING_API_BASE_URL: process.env.ENV_TAGGING_API_BASE_URL || 'https://api.topcoder-dev.com',
+  TAGGING_API_BASE_URL: process.env.TAGGING_API_BASE_URL || 'https://api.topcoder-dev.com',
 
   M2M_AUDIT_USER_ID: process.env.M2M_AUDIT_USER_ID || -1,
   M2M_AUDIT_HANDLE: process.env.M2M_AUDIT_HANDLE || 'TopcoderService'
